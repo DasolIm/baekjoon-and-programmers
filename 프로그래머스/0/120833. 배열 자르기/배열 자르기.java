1509d013e39a408fbac07c639305e4b33ce15280
dasol <121621478+DasolIm@users.noreply.github.com>
2024-04-12 23:17:22 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -2,10 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int [] new_numbers = new int[num2-num1+1];
-        for (int i=num1; i<=num2; i++) {
-            new_numbers[i - num1] = numbers[i];
-        }
-        return new_numbers;
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
