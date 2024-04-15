c7e52dbb6e92600d547da3834b6b5f1e73e018ec
dasol <121621478+DasolIm@users.noreply.github.com>
2024-04-15 16:15:09 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int [] resArr = new int[2];
+        int max = 0; int idx = 0;
+        for (int i=0; i<array.length; i++) {
+            if (max < array[i]) { max = array[i]; idx = i; }
+        }
+        resArr[0] = max; resArr[1] = idx;
+        return resArr;
+    }
+}
