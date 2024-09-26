c3ff2460d43d6558348050ea2da8fbcfb2a47bf2
dasol <121621478+DasolIm@users.noreply.github.com>
2024-09-26 10:14:29 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/README.md
+++ 프로그래머스/0/181929. 원소들의 곱과 합/README.md
@@ -1,10 +1,10 @@
 # [level 0] 원소들의 곱과 합 - 181929 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181929) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181929?language=java) 
 
 ### 성능 요약
 
-메모리: 70.8 MB, 시간: 0.01 ms
+메모리: 72.9 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 02일 00:07:46
+2024년 09월 26일 10:14:22
 
 ### 문제 설명
 
--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] num_list) {
-        int mul = 1; int sum = 0;
-        for (int i=0; i<num_list.length; i++) {
-            mul *= num_list[i]; sum += num_list[i];
+        int a = 1; int b = 0;
+        for (int i : num_list) {
+            a *= i;
+            b += i;
         }
-        sum *= sum;
-        return (mul > sum) ? 0 : 1;
+        return (b*b > a) ? 1 : 0; 
     }
 }
